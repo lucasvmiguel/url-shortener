d0fd3d9804b72e254ea9ff0c6c92f3f33614bcf3
Lucas Vieira <lucas.miguel@b2wdigital.com>
2017-06-22 08:18:55 -0300

    layout improved

--- client/src/components/ShortenForm.jsx
+++ client/src/components/ShortenForm.jsx
@@ -6,19 +6,19 @@ class ShortenForm extends React.Component {
     super(props);
     this.state = {url: this.props.url};
 
-    this.handleChange = this.handleChange.bind(this);
+    // this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
+    // const target = event.target;
+    // const value = target.type === 'checkbox' ? target.checked : target.value;
+    // const name = target.name;
 
-    const newState = this.state;
-    newState.url[name] = value;
-    this.setState(newState);
-    this.props.onChange(newState.url);
+    // const newState = this.state;
+    // newState.url[name] = value;
+    // this.setState(newState);
+    // this.props.onChange(newState.url);
   }
 
   handleSubmit(event) {
@@ -28,12 +28,13 @@ class ShortenForm extends React.Component {
   render() {
     return (
       <form className='flex-grid padding-top padding-top-section' onSubmit={this.handleSubmit}>
-        <input className="form-input-text col-0-9" type="text" name="url" value={this.state.url} onChange={this.handleChange} />
+        <input className="form-input-text col-0-9" type="text" name="url" />
         <input className="form-input-submit col-0-2" type="submit" value="Shorten this link" />
       </form>
     );
   }
 }
+// <input className="form-input-text col-0-9" type="text" name="url" value={this.state.url} onChange={this.handleChange} />
 
 ShortenForm.propTypes = {
   onClick: PropTypes.func.isRequired,
--- client/src/components/ShortenItem.jsx
+++ client/src/components/ShortenItem.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import {timeSinceString} from '../services/datetime.service.js';
+
 const ShortenItem = ({appUrl, urlShorten}) => {
   return (
     <div className='flex-grid'>
       <div className='col-0-8 block-grid'>
-        <p>
-          <a href="#">{appUrl}/{urlShorten.urlShorten}</a> <a href="#">Click to copy this link</a>
+        <p className="furfles">
+          <strong>
+            <a className='url-domain-link' href={`${appUrl}/${urlShorten.urlShorten}`} target="_blank">{appUrl}/</a>
+            <a className='url-shorten-link' href={`${appUrl}/${urlShorten.urlShorten}`} target="_blank">{urlShorten.urlShorten}</a>
+          </strong> 
+          <a className='click-to-copy-link' href="#">Click to copy this link</a>
         </p>
-        <label>{urlShorten.urlExtended}</label>
+        <label className='url-extended-link'>{urlShorten.urlExtended}</label>
       </div>
       <div className='col-0-1 shorten-list-item-info align-center'>{urlShorten.visits}</div>
-      <div className='col-0-2 shorten-list-item-info align-center'>{urlShorten.lastVisited}</div>
+      <div className='col-0-2 shorten-list-item-info align-center'>{timeSinceString(urlShorten.lastVisited)}</div>
     </div>
   );
 }
